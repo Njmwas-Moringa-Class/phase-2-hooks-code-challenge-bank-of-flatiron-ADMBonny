3a791904addd4d849c658fd3acbc0d853a4a91c9
Bonventure Maeta Oanda <64786006+ADMBonny@users.noreply.github.com.>
2023-12-14 05:39:43 +0300

    Attempted assignment.

--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,14 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 
-function AddTransactionForm() {
+function AddTransactionForm({ addTransaction }) {
+  const [transactionData, setTransactionData] = useState({
+    date: "",
+    description: "",
+    category: "",
+    amount: 0,
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setTransactionData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    if (
+      transactionData.date &&
+      transactionData.description &&
+      transactionData.category &&
+      transactionData.amount
+    ) {
+      
+      addTransaction(transactionData);
+
+      
+      setTransactionData({
+        date: "",
+        description: "",
+        category: "",
+        amount: 0,
+      });
+    }
+  };
+
   return (
     <div className="ui segment">
-      <form className="ui form">
+      <form className="ui form" onSubmit={handleSubmit}>
         <div className="inline fields">
-          <input type="date" name="date" />
-          <input type="text" name="description" placeholder="Description" />
-          <input type="text" name="category" placeholder="Category" />
-          <input type="number" name="amount" placeholder="Amount" step="0.01" />
+          <input
+            type="date"
+            name="date"
+            value={transactionData.date}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="description"
+            placeholder="Description"
+            value={transactionData.description}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="category"
+            placeholder="Category"
+            value={transactionData.category}
+            onChange={handleChange}
+          />
+          <input
+            type="number"
+            name="amount"
+            placeholder="Amount"
+            step="0.01"
+            value={transactionData.amount}
+            onChange={handleChange}
+          />
         </div>
         <button className="ui button" type="submit">
           Add Transaction
